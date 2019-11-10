f61666363e87b8575b4e6763576c799fab513b5c
Yoshiyuki Hirano <yhirano@me.com>
2019-11-10 14:08:57 +0900

    Implement Genre api

--- lib/apple_music/genre.rb
+++ lib/apple_music/genre.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 module AppleMusic
+  class GenreResponse < ResponseRoot; end
+
   # https://developer.apple.com/documentation/applemusicapi/genre
   class Genre < Resource
     def initialize(props = {})
@@ -14,6 +16,34 @@ module AppleMusic
 
       def initialize(props = {})
         @name = props['name'] # required
+      end
+    end
+
+    class << self
+      # e.g. AppleMusic::Genre.find(14)
+      # https://developer.apple.com/documentation/applemusicapi/get_a_catalog_genre
+      # https://developer.apple.com/documentation/applemusicapi/get_a_catalog_song
+      def find(id, **options)
+        store_front = StoreFront.lookup(options.delete(:store_front))
+        response = AppleMusic.get("catalog/#{store_front}/genres/#{id}", options)
+        genre_response = GenreResponse.new(response.body)
+        new(genre_response.data[0])
+      end
+
+      # e.g. AppleMusic::Genre.list
+      # e.g. AppleMusic::Genre.list(ids: [20, 34])
+      # https://developer.apple.com/documentation/applemusicapi/get_catalog_top_charts_genres
+      # https://developer.apple.com/documentation/applemusicapi/get_multiple_catalog_genres
+      def list(**options)
+        if options[:ids]
+          ids = options[:ids].is_a?(Array) ? options[:ids].join(',') : options[:ids]
+          options[:ids] = ids
+        end
+
+        store_front = StoreFront.lookup(options.delete(:store_front))
+        response = AppleMusic.get("catalog/#{store_front}/genres", options)
+        genre_response = GenreResponse.new(response.body)
+        genre_response.data.map { |props| new(props) }
       end
     end
   end
