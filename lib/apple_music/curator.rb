8bf67f9b2baf3eb3238c6a3eaf26c738c654daf3
Yoshiyuki Hirano <yhirano@me.com>
2019-11-10 14:27:32 +0900

    Implement Curator api

--- lib/apple_music/curator.rb
+++ lib/apple_music/curator.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 module AppleMusic
+  class CuratorResponse < ResponseRoot; end
+
   # https://developer.apple.com/documentation/applemusicapi/curator
   class Curator < Resource
     def initialize(props = {})
@@ -25,6 +27,48 @@ module AppleMusic
     class Relationships
       def initialize(props = {})
         @playlists = PlaylistRelationship.new(props['playlists']).data
+      end
+    end
+
+    class << self
+      # e.g. AppleMusic::Curator.find(1107687517)
+      # https://developer.apple.com/documentation/applemusicapi/get_a_catalog_curator
+      def find(id, **options)
+        store_front = StoreFront.lookup(options.delete(:store_front))
+        response = AppleMusic.get("catalog/#{store_front}/curators/#{id}", options)
+        curator_response = CuratorResponse.new(response.body)
+        new(curator_response.data[0])
+      end
+
+      # e.g. AppleMusic::Curator.list(ids: [976439448, 1107687517])
+      def list(**options)
+        raise ParameterMissing, 'required parameter :ids is missing' unless options[:ids]
+
+        get_collection_by_ids(options.delete(:ids), options)
+      end
+
+      # e.g. AppleMusic::Curator.get_collection_by_ids([976439448, 1107687517])
+      # https://developer.apple.com/documentation/applemusicapi/get_multiple_catalog_curators
+      def get_collection_by_ids(ids, **options)
+        ids = ids.is_a?(Array) ? ids.join(',') : ids
+        store_front = StoreFront.lookup(options.delete(:store_front))
+        response = AppleMusic.get("catalog/#{store_front}/curators", options.merge(ids: ids))
+        curator_response = CuratorResponse.new(response.body)
+        curator_response.data.map { |props| new(props) }
+      end
+
+      # e.g. AppleMusic::Curator.get_relationship(976439448, :playlists)
+      # https://developer.apple.com/documentation/applemusicapi/get_a_catalog_curator_s_relationship_directly_by_name
+      def get_relationship(id, relationship_type, **options)
+        store_front = StoreFront.lookup(options.delete(:store_front))
+        response = AppleMusic.get("catalog/#{store_front}/curators/#{id}/#{relationship_type}", options)
+        curator_response = CuratorResponse.new(response.body)
+        curator_response.data.map { |props| Resource.build(props) }
+      end
+
+      # e.g. AppleMusic::Curator.related_playlists(976439448)
+      def related_playlists(id, **options)
+        get_relationship(id, :playlists, options)
       end
     end
   end
--- lib/apple_music/playlist.rb
+++ lib/apple_music/playlist.rb
@@ -14,17 +14,22 @@ module AppleMusic
     # https://developer.apple.com/documentation/applemusicapi/playlist/attributes
     class Attributes
       attr_reader :artwork, :curator_name, :description, :last_modified_date,
-                  :name, :play_params, :playlist_type, :url
+                  :name, :play_params, :playlist_type, :url, :is_chart
 
       def initialize(props = {})
         @artwork = Artwork.new(props['artwork']) if props['artwork']
         @curator_name = props['curatorName']
         @description = EditionalNotes.new(props['description']) if props['description']
-        @last_modified_date = Date.parse(props['lastModifiedDate']) # required
+        @last_modified_date = Date.parse(props['lastModifiedDate']) if props['lastModifiedDate']
         @name = props['name'] # required
         @play_params = PlayParameters.new(props['playParams']) if props['playParams']
         @playlist_type = props['playlistType'] # required
         @url = props['url'] # required
+        @is_chart = props['isChart']
+      end
+
+      def chart?
+        is_chart
       end
 
       def user_shared?
