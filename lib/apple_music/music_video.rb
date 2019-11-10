290cf85e0629cb86b2013443a90050d83ffb160c
Yoshiyuki Hirano <yhirano@me.com>
2019-11-10 12:20:39 +0900

    Implement MusicVideo api

--- lib/apple_music/music_video.rb
+++ lib/apple_music/music_video.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 module AppleMusic
+  class MusicVideoResponse < ResponseRoot; end
+
   # https://developer.apple.com/documentation/applemusicapi/musicvideo
   class MusicVideo < Resource
     def initialize(props = {})
@@ -52,6 +54,73 @@ module AppleMusic
         @albums = AlbumRelationship.new(props['albums']).data
         @artists = ArtistRelationship.new(props['artists']).data
         @genres = GenreRelationship.new(props['genres']).data
+      end
+    end
+
+    class << self
+      # e.g. AppleMusic::MusicVideo.find(401135199)
+      # https://developer.apple.com/documentation/applemusicapi/get_a_catalog_music_video
+      def find(id, **options)
+        store_front = StoreFront.lookup(options.delete(:store_front))
+        response = AppleMusic.get("catalog/#{store_front}/music-videos/#{id}", options)
+        music_video_response = MusicVideoResponse.new(response.body)
+        new(music_video_response.data[0])
+      end
+
+      # e.g. AppleMusic::MusicVideo.list(ids: [401135199, 401147268])
+      # e.g. AppleMusic::MusicVideo.list(isrc: 'GBDCE0900012')
+      def list(**options)
+        if options[:ids]
+          get_collection_by_ids(options.delete(:ids), options)
+        elsif options[:isrc]
+          get_collection_by_isrc(options.delete(:isrc), options)
+        else
+          raise ParameterMissing, 'required parameter :ids or :isrc is missing'
+        end
+      end
+
+      # e.g. AppleMusic::MusicVideo.get_collection_by_ids([401135199, 401147268])
+      # https://developer.apple.com/documentation/applemusicapi/get_multiple_catalog_music_videos_by_id
+      def get_collection_by_ids(ids, **options)
+        ids = ids.is_a?(Array) ? ids.join(',') : ids
+        store_front = StoreFront.lookup(options.delete(:store_front))
+        response = AppleMusic.get("catalog/#{store_front}/music-videos", options.merge(ids: ids))
+        music_video_response = MusicVideoResponse.new(response.body)
+        music_video_response.data.map { |props| new(props) }
+      end
+
+      # e.g. AppleMusic::MusicVideo.get_collection_by_isrc('GBDCE0900012')
+      # https://developer.apple.com/documentation/applemusicapi/get_multiple_catalog_music_videos_by_isrc
+      def get_collection_by_isrc(isrc, **options)
+        isrc = isrc.is_a?(Array) ? isrc.join(',') : isrc
+        store_front = StoreFront.lookup(options.delete(:store_front))
+        response = AppleMusic.get("catalog/#{store_front}/music-videos", options.merge('filter[isrc]': isrc))
+        music_video_response = MusicVideoResponse.new(response.body)
+        music_video_response.data.map { |props| new(props) }
+      end
+
+      # e.g. AppleMusic::MusicVideo.get_relationship(401135199, :albums)
+      # https://developer.apple.com/documentation/applemusicapi/get_a_catalog_music_video_s_relationship_directly_by_name
+      def get_relationship(id, relationship_type, **options)
+        store_front = StoreFront.lookup(options.delete(:store_front))
+        response = AppleMusic.get("catalog/#{store_front}/music-videos/#{id}/#{relationship_type}", options)
+        music_video_response = MusicVideoResponse.new(response.body)
+        music_video_response.data.map { |props| Resource.build(props) }
+      end
+
+      # e.g. AppleMusic::MusicVideo.related_albums(401135199)
+      def related_albums(id, **options)
+        get_relationship(id, :albums, options)
+      end
+
+      # e.g. AppleMusic::MusicVideo.related_artists(401135199)
+      def related_artists(id, **options)
+        get_relationship(id, :artists, options)
+      end
+
+      # e.g. AppleMusic::MusicVideo.related_genres(401135199)
+      def related_genres(id, **options)
+        get_relationship(id, :genres, options)
       end
     end
   end
