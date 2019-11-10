23db3ed860a85aa707abdc771ce60b252bb16779
Yoshiyuki Hirano <yhirano@me.com>
2019-11-10 19:14:00 +0900

    Remove currently needless classes

--- lib/apple_music/history.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-module AppleMusic
-  class History < Resource
-  end
-end
--- lib/apple_music/rating.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-module AppleMusic
-  class Rating < Resource
-  end
-end
