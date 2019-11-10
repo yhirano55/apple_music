1e5a1bfd160c6d7a0a4da60e686a62030676a9b5
Yoshiyuki Hirano <yhirano@me.com>
2019-11-10 11:22:12 +0900

    Implement Artist api

--- lib/apple_music/artist.rb
+++ lib/apple_music/artist.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 module AppleMusic
+  class ArtistResponse < ResponseRoot; end
+
   # https://developer.apple.com/documentation/applemusicapi/artist
   class Artist < Resource
     def initialize(props = {})
@@ -33,6 +35,50 @@ module AppleMusic
         @music_videos = MusicVideoRelationship.new(props['musicVideos']).data
         @playlists = PlaylistRelationship.new(props['playlists']).data
         @station = StationRelationship.new(props['station']).data.first
+      end
+    end
+
+    class << self
+      # https://developer.apple.com/documentation/applemusicapi/get_a_catalog_artist
+      def find(id, **options)
+        store_front = StoreFront.lookup(options.delete(:store_front))
+        response = AppleMusic.get("catalog/#{store_front}/artists/#{id}", options)
+        artist_response = ArtistResponse.new(response.body)
+        new(artist_response.data[0])
+      end
+
+      # https://developer.apple.com/documentation/applemusicapi/get_multiple_catalog_artists
+      def list(**options)
+        raise(ParameterMissing, 'required parameter :ids is missing') unless options[:ids]
+
+        store_front = StoreFront.lookup(options.delete(:store_front))
+        response = AppleMusic.get("catalog/#{store_front}/artists", options)
+        artist_response = ArtistResponse.new(response.body)
+        artist_response.data.map { |props| new(props) }
+      end
+
+      # https://developer.apple.com/documentation/applemusicapi/get_a_catalog_artist_s_relationship_directly_by_name
+      def get_relationship(id, relationship_type, **options)
+        store_front = StoreFront.lookup(options.delete(:store_front))
+        response = AppleMusic.get("catalog/#{store_front}/artists/#{id}/#{relationship_type}", options)
+        artist_response = ArtistResponse.new(response.body)
+        artist_response.data.map { |props| Resource.build(props) }
+      end
+
+      def related_albums(id, **options)
+        get_relationship(id, :albums, options)
+      end
+
+      def related_genres(id, **options)
+        get_relationship(id, :genres, options)
+      end
+
+      def related_playlists(id, **options)
+        get_relationship(id, :playlists, options)
+      end
+
+      def related_station(id, **options)
+        get_relationship(id, :station, options).first
       end
     end
   end
